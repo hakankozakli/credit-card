517a0f2e57523af024c62cc60207dda3bdba303a
Hakan Kozaklı <hakankozakli@gmail.com>
2018-02-14 19:42:15 +0300

    Update CardExpirationMonth.php

--- src/CardExpirationMonth.php
+++ src/CardExpirationMonth.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Validation\Rule;
 
 class CardExpirationMonth implements Rule
 {
-    const MSG_CARD_EXPIRATION_MONT_INVALID = 'validation.credit_card.card_expiation_month_invalid';
+    const MSG_CARD_EXPIRATION_MONT_INVALID = 'Invalid expiration date';
 
     /**
      * Year field name.
